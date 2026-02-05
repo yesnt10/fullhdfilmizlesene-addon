6b50555bd990a68f36965aaaf8e7e7c29a92547d
yesnt10 <114935167+yesnt10@users.noreply.github.com>
2026-02-06 01:54:49 +0300

    Update addon.js

--- addon.js
+++ addon.js
@@ -1,42 +1,26 @@
-import express from "express";
-import { addonBuilder, serveHTTP } from "stremio-addon-sdk";
-import { getCatalog } from "./scraper.js";
-import { resolveStreams } from "./resolver.js";
+const { addonBuilder } = require("stremio-addon-sdk");
 
 const manifest = {
-  id: "org.fullhdfilm.addon",
-  version: "1.0.0",
-  name: "FullHD Film Addon",
-  description: "FullHDFilmizlesene movies",
-  resources: ["catalog", "stream"],
-  types: ["movie"],
-  catalogs: [
-    {
-      type: "movie",
-      id: "fullhdfilm",
-      name: "FullHD Filmler"
-    }
-  ]
+    "id": "org.fullhdfilmizlesene.addon",
+    "version": "1.0.0",
+    "name": "FullHDFilmizlesene",
+    "description": "Watch movies from fullhdfilmizlesene.tv",
+    "resources": ["catalog", "stream", "meta"],
+    "types": ["movie"],
+    "catalogs": [
+        {
+            "type": "movie",
+            "id": "fhf_new",
+            "name": "FullHDFilmizlesene Yeni",
+            "extra": [
+                { "name": "search", "isRequired": false },
+                { "name": "skip", "isRequired": false }
+            ]
+        }
+    ],
+    "idPrefixes": ["fhf:"]
 };
 
 const builder = new addonBuilder(manifest);
 
-builder.defineCatalogHandler(async () => {
-  const metas = await getCatalog(1);
-  return { metas };
-});
-
-builder.defineStreamHandler(async ({ id }) => {
-  const streams = await resolveStreams(id);
-  return { streams };
-});
-
-const addonInterface = builder.getInterface();
-
-const app = express();
-serveHTTP(addonInterface, { app });
-
-const PORT = 7000;
-app.listen(PORT, () => {
-  console.log(`Addon running on http://localhost:${PORT}/manifest.json`);
-});
+module.exports = builder;
